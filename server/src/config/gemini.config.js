32849e033c2693d6bcf2cfabf1b1677521e705f2
Shiva2355 <sivaganga20005@gmail.com>
2026-06-11 10:54:20 +0530

    first commit

---
+++ server/src/config/gemini.config.js
@@ -0,0 +1,18 @@
+const generateContent = async(prompt, retries = 3) => {
+    for (let i = 0; i < retries; i++) {
+        try {
+            const response = await ai.models.generateContent({
+                model: "gemini-2.5-flash",
+                contents: prompt,
+            });
+
+            return response.text;
+        } catch (error) {
+            console.error(error);
+
+            if (i === retries - 1) throw error;
+
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+        }
+    }
+};
