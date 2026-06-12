d10a5a2c356f609a41a6d6ee25295527d01f7181
Shiva2355 <144972978+Shiva2355@users.noreply.github.com>
2026-06-12 13:02:19 +0530

    Update gemini.config.js

--- server/src/config/gemini.config.js
+++ server/src/config/gemini.config.js
@@ -1,19 +1,26 @@
-const generateContent = async(prompt, retries = 3) => {
-    for (let i = 0; i < retries; i++) {
-        try {
-            const response = await ai.models.generateContent({
-                model: "gemini-2.5-flash",
-                contents: prompt,
-            });
+import { GoogleGenAI } from "@google/genai";
 
-            return response.text;
-        } catch (error) {
-            console.error(error);
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
 
-            if (i === retries - 1) throw error;
+const generateContent = async (prompt, retries = 3) => {
+  for (let i = 0; i < retries; i++) {
+    try {
+      const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: prompt,
+      });
 
-            await new Promise((resolve) => setTimeout(resolve, 3000));
-        }
+      return response.text;
+    } catch (error) {
+      console.error(error);
+
+      if (i === retries - 1) throw error;
+
+      await new Promise((resolve) => setTimeout(resolve, 3000));
     }
+  }
 };
+
 export { generateContent };
